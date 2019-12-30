2a9996067d27b2e52d757a0f3db18c22442e68f6
Troy Halpin <troy.halpin@procore.com>
2019-12-30 01:57:46 -0500

    pin version to 2.9.0 in import

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak@v2.9.0/mod.ts";
 import { APP_HOST, APP_PORT } from "./config.ts";
 import router from "./routing.ts";
 import notFound from "./handlers/notFound.ts";
